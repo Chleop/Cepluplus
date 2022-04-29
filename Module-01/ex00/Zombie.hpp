4fbc38b214f5769e0ed684051857ee95e6d49ac5
Chleo PROESCHEL <cproesch@e3r11p10.clusters.42paris.fr>
2022-04-29 10:06:03 +0200

    dd

---
+++ Module-01/ex00/Zombie.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cproesch <cproesch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/29 10:00:27 by cproesch          #+#    #+#             */
+/*   Updated: 2022/04/29 10:05:52 by cproesch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+# include <iomanip>
+# include <string>
+# include <fstream>
+
+// ************************************************************************** //
+//                                     CLASS                                  //
+// ************************************************************************** //
+
+class Zombie {
+
+
+public:
+
+// Fonctions non-membres
+
+// Constructeur et destructeur
+
+// Fonctions membres
+
+private:
+
+// Attributs non-membres
+
+// Fonctions non-membres
+
+// Fonctions membres
+
+// Attributs membres
+
+};
+
+
+#endif
