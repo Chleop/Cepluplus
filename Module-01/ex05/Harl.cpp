89a28d816b2c5b086a68867db843265e01531948
Chleo PROESCHEL <cproesch@e3r10p9.42.fr>
2022-05-06 11:09:44 +0200

    sending ex06

--- Module-01/ex05/Harl.cpp
+++ Module-01/ex05/Harl.cpp
@@ -34,25 +34,45 @@ Harl::~Harl(void)
 // MEMBER
 void    Harl::debug(void)
 {
-    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+    std::cout   << "[ DEBUG ]"
+                << std::endl
+                << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger."
+                << std::endl 
+                << "I really do!" 
+                << std::endl
+                << std::endl;
     return;
 }
 
 void    Harl::info(void)
 {
-    std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+    std::cout   << "[ INFO ]"
+                << std::endl
+                << "I cannot believe adding extra bacon costs more money."
+                << "You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!"
+                << std::endl
+                << std::endl;
     return;
 }
 
 void    Harl::warning(void)
 {
-    std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
+    std::cout   << "[ WARNING ]"
+                << std::endl
+                << "I think I deserve to have some extra bacon for free."
+                << "\nI've been coming for years whereas you started working here since last month."
+                << std::endl
+                << std::endl;
     return;
 }
 
 void    Harl::error(void)
 {
-    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+    std::cout   << "[ ERROR ]"
+                << std::endl
+                << "This is unacceptable! I want to speak to the manager now."
+                << std::endl
+                << std::endl;
     return;
 }
 
