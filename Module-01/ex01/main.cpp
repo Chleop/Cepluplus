d19ccc5ddb83a5776454fd03850e13950fe2cbcc
Chleo PROESCHEL <cproesch@e3r10p9.42.fr>
2022-05-06 11:35:51 +0200

    d

--- Module-01/ex01/main.cpp
+++ Module-01/ex01/main.cpp
@@ -6,13 +6,13 @@
 /*   By: cproesch <cproesch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 09:59:11 by cproesch          #+#    #+#             */
-/*   Updated: 2022/05/04 15:18:59 by cproesch         ###   ########.fr       */
+/*   Updated: 2022/05/06 11:30:10 by cproesch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-# define NUMBER_OF_TESTS    4
+# define NUMBER_OF_TESTS    3
 
 std::ostream& bold_on(std::ostream& os)
 {
@@ -27,14 +27,23 @@ std::ostream& bold_off(std::ostream& os)
 int main(void)
 {
     Zombie      *x;
-    std::string name[NUMBER_OF_TESTS] = {"Marie", "Joseph", "Jesus", "Madeleine"};
-    int         N[NUMBER_OF_TESTS] = {1, 0, 10, 3};
+    std::string name[NUMBER_OF_TESTS] = {"Jesus", "Marie", "Joseph"};
+    int         N[NUMBER_OF_TESTS] = {10, 1, 0};
     int         i;
     int         j;
 
     for (i = 0; i < NUMBER_OF_TESTS; i++)
     {
-        std::cout << bold_on << "Test " << i << " : Here is a Horde of " << N[i] << " Zombies who's names are all " << name[i] << bold_off << std::endl;
+        std::cout   << std::endl
+                    << bold_on
+                    << "Test "
+                    << i 
+                    << " : Here is a Horde of " 
+                    << N[i] 
+                    << " Zombie(s) who's names are all " 
+                    << name[i] 
+                    << bold_off 
+                    << std::endl;
         x = zombieHorde(N[i], name[i]);
         for (j = 0; j < N[i]; j++)
             x[j].announce();
