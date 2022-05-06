92cb705b5ceb3c864f944173d5dd14c775307ead
Chleo PROESCHEL <cproesch@e3r10p9.42.fr>
2022-05-06 12:03:27 +0200

    dd

--- Module-01/ex05/main.cpp
+++ Module-01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cproesch <cproesch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 09:59:11 by cproesch          #+#    #+#             */
-/*   Updated: 2022/05/05 19:10:41 by cproesch         ###   ########.fr       */
+/*   Updated: 2022/05/06 12:02:40 by cproesch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,19 @@ std::ostream& bold_off(std::ostream& os)
 int main(void)
 {
     Harl h;
+
     std::cout << bold_on << "\nTesting DEBUG:" << bold_off << std::endl;
     h.complain("DEBUG");
+
     std::cout << bold_on << "\nTesting INFO:" << bold_off << std::endl;
     h.complain("INFO");
+
     std::cout << bold_on << "\nTesting WARNING:" << bold_off << std::endl;
     h.complain("WARNING");
+
     std::cout << bold_on << "\nTesting ERROR:" << bold_off << std::endl;
     h.complain("ERROR");
+    
     std::cout << bold_on << "\nTesting ERfffROR:" << bold_off << std::endl;
     h.complain("ERfffROR");
     std::cout << std::endl;
