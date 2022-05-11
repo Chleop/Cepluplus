ad1586679da9006487aee785965d97f5ef4cc74c
Chleo PROESCHEL <cproesch@e3r10p9.42.fr>
2022-05-11 12:11:22 +0200

    petit pb de compil...

--- Module-02/ex00/Fixed.hpp
+++ Module-02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: cproesch <cproesch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 18:20:25 by cproesch          #+#    #+#             */
-/*   Updated: 2022/05/10 15:35:50 by cproesch         ###   ########.fr       */
+/*   Updated: 2022/05/11 12:10:01 by cproesch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ public:
 private:
 // Attributs membres
     int                 _x;
-    static const int    _y;
+    static const int    _y = 8;
     // A static data member of a class is also known as a "class variable", 
     // because there is only one common variable for all the objects of that 
     // same class, sharing the same value: i.e., its value is not different 
@@ -46,7 +46,5 @@ private:
     // properties as non-member variables but they enjoy class scope.
 
 };
-
-const int    Fixed::_y = 8;
 
 #endif
